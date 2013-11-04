5b81168564e1a9a08fc56315bff6601bf35951e3
wzsmith1 <wzsmith1@yahoo.com>
2013-11-04 12:58:54 -0500

    change  1
    changes

---
+++ Sogeti_SampleApplication/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sogeti_SampleApplication
+{
+    public partial class Form1 : Form
+    {
+        private int clicks;
+
+        public Form1()
+        {
+            InitializeComponent();
+            clicks = 0;
+            label2.Text = clicks.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Button Click Successful");
+            clicks += 1;
+            label2.Text = clicks.ToString();
+        }
+    }
+}
